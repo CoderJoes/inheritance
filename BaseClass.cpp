7edd996cc87966f3a0ffcc943a1967200874de9a
Joe Ferrucci <mrferrucci@gmail.com>
2013-04-29 15:14:21 -0700

    Create BaseClass.cpp

---
+++ BaseClass.cpp
@@ -0,0 +1,10 @@
+//
+//  BaseClass.cpp
+//  Scratch_Plate
+//
+//  Created by Joe Ferrucci on 4/28/13.
+//  Copyright (c) 2013 Joseph Ferrucci. All rights reserved.
+//
+
+#include "BaseClass.h"
+int BaseClass::count = 0;
