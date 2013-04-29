fc929dab379481a7b103002a8a20be21d946a709
Joe Ferrucci <mrferrucci@gmail.com>
2013-04-29 15:14:47 -0700

    Create InheritClass1.cpp

---
+++ InheritClass1.cpp
@@ -0,0 +1,9 @@
+//
+//  InheritClass1.cpp
+//  Scratch_Plate
+//
+//  Created by Joe Ferrucci on 4/28/13.
+//  Copyright (c) 2013 Joseph Ferrucci. All rights reserved.
+//
+
+#include "InheritClass1.h"
